32e9f9633bdb3c83351b1f96d893d5920aaf72b9
Laken Harville <rlakenharville@gmail.com>
2022-03-17 13:08:09 -0500

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; } 
+        public int Year { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine($"This guy drives a {Year} {Make} {Model}!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Year = 2013;
+            car.Make = "KIA";
+            car.Model = "Optima";
+            car.Drive();
+
+        }
+
+        
+    }
+}
